5f6bdf1e509f3abb013ecc8f9127597e3d68aa94
yshayy <yshayy@gmail.com>
2017-10-09 20:05:22 +0300

    first version

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-tweek';
+import { createTweekClient, watchVersion } from 'tweek-client';
+import TweekLocalCache from 'tweek-local-cache';
+import ThemeProvider from './ThemeProvider';
+import App from './App';
+import './index.css';
+import config from '../config.json';
+
+const baseServiceUrl = config.TweekUrl;
+
+function createTweekRepo(){
+  const tweekClient = createTweekClient({ baseServiceUrl });
+  const tweekRepo = new TweekLocalCache({ client: tweekClient });
+  
+  // experimental - for hot reloading
+  const tweekWatcher = watchVersion(baseServiceUrl);
+  tweekWatcher.subscribe(() => tweekRepo.refresh());
+  //
+
+  return tweekRepo;
+}
+
+ReactDOM.render(
+  <Provider repo={createTweekRepo()}>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root'),
+);
